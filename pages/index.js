304ccd1801ffef7848eeb8476d107521bdad356f
Ricpla <ricpla17@gmail.com>
2022-08-25 10:45:05 -0400

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Hero from "../components/UI/Hero";
+import Services from "../components/UI/Services";
+import About from "../components/UI/About";
+import Portfolio from "../components/UI/Portfolio";
+import Testimonial from "../components/UI/Testimonial";
+import Contact from "../components/UI/Contact";
+
+export default function Home() {
+  return (
+    <Fragment>
+      <Hero />
+      <Contact />
+    </Fragment>
+  );
+}
